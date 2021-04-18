991d2e7c1c4997c6440ce6d0781534d1f45deb6b
David <cubsel@hotmail.com>
2021-04-17 20:56:57 -0400

    started technoblades battle attack and enemy ai

--- Assets/Scripts/systems/BattleSystems/EnemyBattleAISystem.cs
+++ Assets/Scripts/systems/BattleSystems/EnemyBattleAISystem.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Entities;
 using UnityEngine;
 
-public class EnemyBattleAISystem : MonoBehaviour
+public class EnemyBattleAISystem : SystemBase
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void OnUpdate()
     {
-        
-    }
+        float deltaTime = Time.DeltaTime;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Entities.ForEach((EnemyBattleData enemyBattleData, BattleData battleData) =>
+        {
+            EnemyAiTypes temp = EnemyAiTypes.random;
+            //have something to remember the ai type and do actions accordingly
+            // have a list of battle attacks that you can choose
+            //ai 1, normal timers, but randomly tick the timer down, and randomly choose an attack
+            //ai 2, normal timer, randomly tick the timer down, but intellegently pick a random attack
+            //ai 3, use a machine learning algorithm
+
+            switch (temp)
+            {
+                case EnemyAiTypes.random:
+
+                    battleData.useTime -= Random.value;
+                    break;
+            }
+            
+        }).Run();
     }
+}
+
+public enum EnemyAiTypes
+{
+    random,
+    inteligentRandom,
+    machine
 }
